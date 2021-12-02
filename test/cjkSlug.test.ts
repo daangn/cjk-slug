da8191a47edd6541b6a7fc93bd8ecfa9aa03bcd8
Hyeseong Kim <tim@daangn.com>
2021-12-02 22:35:35 +0900

    strip quotation marks & apostrophe
    Resolves #3

--- test/cjkSlug.test.ts
+++ test/cjkSlug.test.ts
@@ -7,9 +7,12 @@ const cases: Array<[input: string, output: string]> = [
   ['당근마켓 - 대한민국 1등 동네 커뮤니티', '당근마켓-대한민국-1등-동네-커뮤니티'],
   ['PM/기획', 'pm-기획'],
   ['개발 (iOS / 안드로이드)', '개발-ios-안드로이드'],
+  ['자바스크립트도 \"당근\"이세요?', '자바스크립트도-당근이세요'],
   ['ホロライブプロダクション Hororaibu Purodakushon', 'ホロライブプロダクション-hororaibu-purodakushon'],
-  ['Parse, don’t validate', 'parse-don-t-validate'],
   ['------title------', 'title'],
+  ['Parse, don’t validate', 'parse-dont-validate'],
+  ['Alexis King said "Parse, don\'t validate"', 'alexis-king-said-parse-dont-validate'],
+  ['Someone shout "help, help!", but no one care', 'someone-shout-help-help-but-no-one-care'],
 ];
 
 for (const [input, output] of cases) {
